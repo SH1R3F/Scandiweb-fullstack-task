e9da04ee3383ccd6dba5b07172af656eb894e7f5
Mahmoud Shiref <mahmoud.shiref@scandiweb.com>
2023-08-29 21:41:20 +0300

    Add default homepage

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use Scandiweb\Router;
 
+Router::view('/', 'welcome');
---
+++ views/welcome.php
@@ -0,0 +1,3 @@
+<?php include __DIR__ . '/layout/header.php' ?>
+    <h1 class="text-center">Hello world!</h1>
+<?php include __DIR__ . '/layout/footer.php' ?>
