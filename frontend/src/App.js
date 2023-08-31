2013067f10123d30dc5462f6d2b66b7746596243
Mahmoud Shiref <mahmoud.shiref@scandiweb.com>
2023-08-31 10:14:36 +0300

    Add react router & homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import React from "react";
+import HomePage from "./route/HomePage/HomePage.component";
 
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomePage/>,
+    },
+]);
 
 export class App extends React.PureComponent {
     render() {
         return (
             <div className="App">
-                Hello World!
+                <RouterProvider router={router}/>
             </div>
         );
     }
---
+++ frontend/src/route/HomePage/HomePage.component.jsx
@@ -0,0 +1,12 @@
+import React, {PureComponent} from 'react';
+
+class HomePageComponent extends PureComponent {
+    render() {
+        return (
+            <div>
+                Homepage
+            </div>
+        );
+    }
+}
+export default HomePageComponent;
