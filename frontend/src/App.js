8cc298f433e8ad9defa421dc57c0b2f16701eed8
Mahmoud Shiref <mahmoud.shiref@scandiweb.com>
2023-08-31 10:12:06 +0300

    Create React app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from "react";
+
+
+export class App extends React.PureComponent {
+    render() {
+        return (
+            <div className="App">
+                Hello World!
+            </div>
+        );
+    }
+}
+
+export default App;
