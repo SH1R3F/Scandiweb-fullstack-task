b2192b176479aff2ccbf49a5a5e75f2fe8ac361f
Mahmoud Shiref <mahmoud.shiref@scandiweb.com>
2023-08-31 14:01:35 +0300

    Fix configuring store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,5 +2,7 @@ import {configureStore} from "@reduxjs/toolkit";
 import NavbarReducer from "./Navbar/Navbar.reducer";
 
 export const store = configureStore({
-    navbar: NavbarReducer
+    reducer: {
+        navbar: NavbarReducer
+    }
 })
