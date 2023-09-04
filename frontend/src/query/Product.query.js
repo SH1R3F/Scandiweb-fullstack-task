cc982b25040e447411870a2bc7d7ed355ea365b0
Mahmoud Shiref <mahmoud.shiref@scandiweb.com>
2023-09-04 16:45:57 +0300

    Update the products query to include nested fields

--- frontend/src/query/Product.query.js
+++ frontend/src/query/Product.query.js
@@ -1,33 +1,24 @@
 import {Field} from "../Util/Query";
 
 export class ProductQuery {
-    getQuery() {
+    getCategoryPageQuery() {
         return new Field('products').addFieldList([
             'id',
             'name',
             'inStock',
             'gallery',
             'description',
-            // category {
-            //     name
-            // }
-            // attrs {
-            //     id
-            //     name
-            //     type
-            //     items {
-            //         id
-            //         value
-            //         display_value
-            //     }
-            // }
-            // prices {
-            //     amount
-            //     currency {
-            //         label
-            //         symbol
-            //     }
-            // }
+            new Field('category').addFieldList(['name']),
+            new Field('attrs').addFieldList([
+                'id',
+                'name',
+                'type',
+                new Field('items').addFieldList(['id', 'value', 'display_value'])
+            ]),
+            new Field('prices').addFieldList([
+                'amount',
+                new Field('currency').addFieldList(['label', 'symbol'])
+            ]),
             'brand'
         ]);
     }
