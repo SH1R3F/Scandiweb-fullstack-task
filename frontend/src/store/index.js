1dd934c1cb7c45f6bf933faa4ecff5d645341077
Mahmoud Shiref <mahmoud.shiref@scandiweb.com>
2023-09-04 14:47:22 +0300

    Add the category reducer to global store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
 import NavbarReducer from "./Navbar/Navbar.reducer";
+import CategoryReducer from "./Category/Category.reducer";
 
 export const store = configureStore({
     reducer: {
-        navbar: NavbarReducer
+        navbar: NavbarReducer,
+        category: CategoryReducer
     }
 })
