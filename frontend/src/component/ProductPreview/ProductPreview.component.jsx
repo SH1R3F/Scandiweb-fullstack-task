65f470403514fffa2586cb204bff28a4f33fc615
Mahmoud Shiref <mahmoud.shiref@scandiweb.com>
2023-09-05 11:51:05 +0300

    Add ProductPreview dynamic functionality

--- frontend/src/component/ProductPreview/ProductPreview.component.jsx
+++ frontend/src/component/ProductPreview/ProductPreview.component.jsx
@@ -3,15 +3,24 @@ import React, {PureComponent} from 'react';
 import './ProductPreview.style.scss'
 
 class ProductPreviewComponent extends PureComponent {
+
+    state = {
+        activeIndex: 0
+    }
+
+    setImageActive(index) {
+        this.setState({activeIndex: index})
+    }
+
     render() {
+        const {gallery} = this.props;
+        const {activeIndex} = this.state
+
         return (
-            <div className="ProductPreview">
-                <img className="ProductPreview-Image" src="/product.png" alt="Product"/>
-                <img className="ProductPreview-Image" src="/product.png" alt="Product"/>
-                <img className="ProductPreview-Image" src="/product.png" alt="Product"/>
-                <img className="ProductPreview-Image" src="/product.png" alt="Product"/>
-                <img className="ProductPreview-Image" src="/product.png" alt="Product"/>
-                <img className="ProductPreview-Image ProductPreview-Image_isActive" src="/product.png" alt="Product"/>
+            <div className={`ProductPreview ${gallery.length === 1 && 'ProductPreview_isSingleImage'}`}>
+                {gallery.map((image, i) => {
+                    return <img key={i} className={`ProductPreview-Image ${i === activeIndex && 'ProductPreview-Image_isActive'}`} src={image} alt="Product" onClick={() => this.setImageActive(i)}/>;
+                })}
             </div>
         );
     }
