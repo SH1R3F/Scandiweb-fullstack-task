71e673d352bd9bd54419630b42e7f4ef9549f78d
Mahmoud Shiref <mahmoud.shiref@scandiweb.com>
2023-09-05 11:06:01 +0300

    Implement Product query logic

--- frontend/src/query/Product.query.js
+++ frontend/src/query/Product.query.js
@@ -1,7 +1,27 @@
 import {Field} from "../Util/Query";
 
 export class ProductQuery {
-
+    getQuery(productId) {
+        return new Field('product').addArgument('id', 'ID!', productId).addFieldList([
+            'id',
+            'name',
+            'inStock',
+            'gallery',
+            'description',
+            new Field('category').addFieldList(['name']),
+            new Field('attrs').addFieldList([
+                'id',
+                'name',
+                'type',
+                new Field('items').addFieldList(['id', 'value', 'display_value'])
+            ]),
+            new Field('prices').addFieldList([
+                'amount',
+                new Field('currency').addFieldList(['label', 'symbol'])
+            ]),
+            'brand'
+        ])
+    }
 }
 
 export default new ProductQuery();
